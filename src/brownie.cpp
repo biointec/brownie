8d6384694759af99b429c7b4da694c37d1a38332
Mahdi Heydari <mahdi.heydari@intec.ugent.be>
2016-02-23 12:17:23 +0100

    moving Doxygen comments to brownie.h file

--- src/brownie.cpp
+++ src/brownie.cpp
@@ -33,7 +33,6 @@ using namespace std;
 Brownie::Brownie(int argc, char** args)
 {
         settings.parseCommandLineArguments(argc, args, libraries);
-
         Kmer::setWordSize(settings.getK());
         RKmer::setWordSize(settings.getK() - KMERBYTEREDUCTION * 4);
 }
@@ -42,33 +41,29 @@ void Brownie::printInFile()
 {
         string konsole = settings.getTempDirectory()+ "/konsole.txt";
         freopen(konsole.c_str(), "a", stdout);
-        cout<<"-------------------------------------------------------------------------------"<<endl;
+        cout << "-------------------------------------------------------------------------------" << endl;
         time_t t;
         time(&t);
         cout << ctime(&t) << endl; // e.g., Fri May 02 17:57:14 2003
-        cout<<"-------------------------------------------------------------------------------"<<endl;
+        cout << "-------------------------------------------------------------------------------" << endl;
         cout << "Welcome to Brownie\n" << endl;
 }
-/**
- * this routine estimates initial parameters for the graph
- * by making a test graph.
- *
- * @graph The acutal graph which later will be cleand in this stage. Initial parameters of this
- * graph will be set in this routine based on a test graph manipulation.
- */
-void Brownie::parameterEstimationInStage4(DBGraph &graph){
-        cout <<endl<< " ================ Parameter Estimation ===============" << endl;
-        double  estimatedKmerCoverage=0,estimatedMKmerCoverageSTD=0, cutOffvalue=0, readLength=0;
-        readLength=libraries.getAvgReadLength();
-        if (readLength<=settings.getK() ||readLength>500)
-                readLength=150;
 
+void Brownie::parameterEstimationInStage4(DBGraph &graph){
+        cout << endl << " ================ Parameter Estimation ===============" << endl;
+        double  estimatedKmerCoverage = 0,
+                estimatedMKmerCoverageSTD = 0,
+                cutOffvalue = 0,
+                readLength = 0;
+        readLength = libraries.getAvgReadLength();
+        if (readLength <= settings.getK() || readLength > 500)
+                readLength = 150;
         cout << "Loading test graph for initial parameter estimation" << endl;
         DBGraph testgraph(settings);
         testgraph.loadGraphBin(getBinNodeFilename(3),
                                getBinArcFilename(3),
                                getMetaDataFilename(3));
-        testgraph.readLength=readLength;
+        testgraph.readLength = readLength;
         #ifdef DEBUG
         testgraph.compareToSolution(getTrueMultFilename(3), true);
         #endif
@@ -82,14 +77,14 @@ void Brownie::parameterEstimationInStage4(DBGraph &graph){
 
         estimatedKmerCoverage = testgraph.estimatedKmerCoverage;
         estimatedMKmerCoverageSTD = testgraph.estimatedMKmerCoverageSTD;
-        double estimatedErroneousKmerCoverage = 1+estimatedKmerCoverage/100;
+        double estimatedErroneousKmerCoverage = 1+ estimatedKmerCoverage/100;
         double e = 2.718281;
         double c = estimatedErroneousKmerCoverage/estimatedKmerCoverage;
 
         if (settings.getCutOffValue() > 0)
-                cutOffvalue=settings.getCutOffValue();
+                cutOffvalue = settings.getCutOffValue();
         else
-                cutOffvalue =(estimatedErroneousKmerCoverage-estimatedKmerCoverage)* (log(e)/log(c));
+                cutOffvalue = (estimatedErroneousKmerCoverage-estimatedKmerCoverage)* (log(e)/log(c));
         testgraph.updateGraphSize();
         testgraph.clear();
            //initialize values for graph parameter based on test graph.
@@ -286,8 +281,6 @@ void Brownie::stageFour()
 
 #ifdef DEBUG
         graph.sanityCheck();
-        string command="pdftk "+settings.getTempDirectory()+ "cov/*.pdf cat output allpdfFiles.pdf";
-        system(command.c_str());
 #endif
         graph.clear();
         cout << "Stage 4 finished.\n" << endl;
